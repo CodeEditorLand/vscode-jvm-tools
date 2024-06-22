f5b93d56b18b815fa0b72ca0a55f863560108cca
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-22 17:06:50 +0300

    squash!

--- jvm-monitor-server/src/test/java/com/microsoft/jvmms/test/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.jvmms.test;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeExampleResourceIT extends JVMListTest {
-
-    // Execute the same tests but in native mode.
-}
